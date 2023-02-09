4de0207d1d8fc870b2b87c022f577d56ba64e118
Connor <cgbroadfoot@gmail.com>
2023-02-09 02:11:11 +0000

    Add data and queries to database

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (name, email, password) VALUES 
+('borsh', 'b@c.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+),
+('Jay', 'das@ls.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+), 
+('Gurtrude', 'zixi@ls.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+);
+
+INSERT INTO properties (owner_id, title, thumbn) VALUES 
+('borsh', 'b@c.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+),
+('Jay', 'das@ls.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+), 
+('Gurtrude', 'zixi@ls.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+);
+
+
